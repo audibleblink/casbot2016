84a5a0f3007dd1f74c2e47f11b68a425fd28c22f
Alex Flores <eflores@mdsol.com>
2015-12-21 17:55:02 -0500

    port over old image search module

--- bot.js
+++ bot.js
@@ -1,8 +1,9 @@
-const Botkit = require('botkit');
+const Botkit = require('botkit')
+const imageSearch = require('./lib/image_search')
 
 const controller = Botkit.slackbot({
   debug: true
-});
+})
 
 controller.spawn({
   token: process.env.SLACK_TOKEN
@@ -11,10 +12,13 @@ controller.spawn({
 
 
 controller.hears('hello', 'direct_message,direct_mention,mention', function(bot, message) {
-  bot.reply(message, 'Hello yourself.');
-});
+  bot.reply(message, 'Hello yourself.')
+})
 
 
 controller.hears('!img', 'ambient', function(bot, message) {
-  bot.reply(message, 'WIP: Fetches images from a Google image search');
-});
+  const query = message.text.replace(/^!img\W/, '')
+  imageSearch(query)
+    .then((link) => bot.reply(message, link))
+    .catch((err) => bot.reply(message, err))
+})
