428677eab1201d8c378bdb40a2cfeee606908cbb
Alex Flores <eflores@mdsol.com>
2015-12-21 19:35:11 -0500

    adds urban dictionary functionality

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Botkit = require('botkit')
 const imageSearch = require('./lib/image_search')
+const urban = require('./lib/urban')
 
 const controller = Botkit.slackbot({
   debug: true
@@ -11,14 +12,27 @@ controller.spawn({
 
 
 
-controller.hears('hello', 'direct_message,direct_mention,mention', function(bot, message) {
+controller.hears('hello', 'direct_message,direct_mention,mention', (bot, message) => {
   bot.reply(message, 'Hello yourself.')
 })
 
-
-controller.hears('!img', 'ambient', function(bot, message) {
+// image search
+controller.hears('!img', 'ambient', (bot, message) => {
   const query = message.text.replace(/^!img\W/, '')
   imageSearch(query)
     .then((link) => bot.reply(message, link))
     .catch((err) => bot.reply(message, err))
+})
+
+// urban dictionary search
+controller.hears('!urban', 'ambient', (bot, message) => {
+  const query = message.text.replace(/^!img\W/, '')
+  urban(query)
+    .then((link) => bot.reply(message, link))
+    .catch((err) => bot.reply(message, err))
+})
+
+// used for checking status
+controller.hears('!ping', 'ambient', (bot, message) => {
+  bot.reply(message, "Pong!")
 })
