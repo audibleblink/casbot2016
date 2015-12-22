3b15df4ca108815cdb7fe8fe2b3f60d3d84deced
Alex Flores <eflores@mdsol.com>
2015-12-21 20:36:11 -0500

    add xbox live status checking

--- bot.js
+++ bot.js
@@ -26,7 +26,7 @@ controller.hears('!img', 'ambient', (bot, message) => {
 
 // urban dictionary search
 controller.hears('!urban', 'ambient', (bot, message) => {
-  const query = message.text.replace(/^!img\W/, '')
+  const query = message.text.replace(/^!urban\W/, '')
   urban(query)
     .then((link) => bot.reply(message, link))
     .catch((err) => bot.reply(message, err))
@@ -35,4 +35,14 @@ controller.hears('!urban', 'ambient', (bot, message) => {
 // used for checking status
 controller.hears('!ping', 'ambient', (bot, message) => {
   bot.reply(message, "Pong!")
+})
+
+// xbox live status checker
+controller.hears('!live', 'ambient', (bot, message) => {
+  const gamertag = message.text.replace(/^!live\W/, '')
+  XBL.getXuid(gamertag)
+      .then(XBL.getPresence)
+      .then((presence) => XBL.prepareResponse(presence, gamertag))
+      .then((response) => bot.reply(message, response))
+      .catch((err) => bot.reply(message, new Error(err)))
 })
