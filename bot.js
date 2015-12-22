f99032f4ab40a928e6fa2c07a751974fc0b28d3f
Alex Flores <eflores@mdsol.com>
2015-12-22 12:57:09 -0500

    slash commands and sane logging
      - botkit is currently broken. sent a PR out to
        fix. will reimnplement once it's fixed on
    master

--- bot.js
+++ bot.js
@@ -14,6 +14,20 @@ controller.spawn({
   token: process.env.SLACK_TOKEN
 }).startRTM()
 
+controller.setupWebserver(5000, (err,express_webserver) => {
+  controller.createWebhookEndpoints(express_webserver)
+})
+
+// // TODO: botkit is currently broken and needs this to implement slash commands
+// // I've submitted a PR and will uncomment and merge once they accept
+
+// controller.storage.teams.save({id: "T03E23VAN", team_domain: "redditcasual"})
+//
+// controller.on('slash_command', (bot,message) => {
+//   // bot.replyPublic(message,'Everyone can see this part of the slash command');
+//   bot.replyPrivate(message,'Only the person who used the slash command can see this.');
+// })
+
 
 // image search
 controller.hears('!img', 'ambient', (bot, message) => {
@@ -46,6 +60,10 @@ controller.hears('!live', 'ambient', (bot, message) => {
       .catch((err) => bot.reply(message, new Error(err)))
 })
 
+// TODO: implemet sane logging functionality
+// controller.hears(".*", 'ambient', (bot, message) => {
+//   controller.storage.channels.save(message)
+// })
 
 
 const stripKeyword = (message) => {
