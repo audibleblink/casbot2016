85a4fe96f94761f084003207f9bbb10c18647c17
Alex Flores <eflores@mdsol.com>
2015-12-23 23:02:46 -0500

    add redis wrapper for botkit

--- bot.js
+++ bot.js
@@ -1,13 +1,14 @@
 "use strict"
 
 const Botkit      = require('botkit')
+const redisStore  = require('./lib/redis_storage')
 const { parsedUptime, stripKeyword } = require('./lib/bot_tools')
 const { imageSearch, urban, XBL }    = require('./lib/bot_plugins')
 
 
 const controller = Botkit.slackbot({
   debug: process.env.NODE_ENV === "development",
-  json_file_store: "./db.json"
+  storage: new redisStore()
 })
 
 controller.spawn({
@@ -18,10 +19,9 @@ controller.setupWebserver(5000, (err,express_webserver) => {
   controller.createWebhookEndpoints(express_webserver)
 })
 
-
 // image search
 controller.hears('!img|!gif', 'ambient', (bot, message) => {
-  const query = stripKeyword(message)
+  const query   = stripKeyword(message)
   const keyword = message.text.match(/^.{4}/)[0]
   imageSearch( keyword === '!gif' ? `gif ${query}` : query )
     .then((link) => bot.reply(message, link))
